5c933a21b5ae82d5f7c73b0a202c7f416cefd08a
Kyle Coffey <mariorules24@gmail.com>
2021-12-04 16:03:48 -0600

    actual compilation, RAII

--- inc/Engine/Shader.hpp
+++ inc/Engine/Shader.hpp
@@ -10,8 +10,15 @@ class Shader
 	GLuint m_id;
 
 	explicit Shader(GLuint id);
+	void cleanup() const;
 
   public:
+	~Shader();
+	Shader(const Shader&) = delete;
+	Shader& operator=(const Shader&) = delete;
+	Shader(Shader&& other) noexcept;
+	Shader& operator=(Shader&& other) noexcept;
+
 	static Shader fromFile(std::string_view path);
 };
 } // namespace phyto::engine
--- src/Engine/Shader.cpp
+++ src/Engine/Shader.cpp
@@ -1,5 +1,7 @@
 #include "Engine/Shader.hpp"
 #include "CGLSL/Parser.hpp"
+#include <sstream>
+#include <vector>
 
 phyto::engine::Shader::Shader(GLuint id) : m_id{id}
 {
@@ -9,5 +11,88 @@ phyto::engine::Shader phyto::engine::Shader::fromFile(std::string_view path)
 {
 	cglsl::ShaderSource sources = cglsl::Parser{}.parse(path);
 
-	return Shader{0};
+	GLuint vertex = glCreateShader(GL_VERTEX_SHADER);
+	const char* vertexSource = sources.vertex.c_str();
+	int vertexLength = (int)sources.vertex.size();
+	glShaderSource(vertex, 1, &vertexSource, &vertexLength);
+	glCompileShader(vertex);
+	int success;
+	glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+	if (success == GL_FALSE)
+	{
+		int infoLogLength;
+		glGetShaderiv(vertex, GL_INFO_LOG_LENGTH, &infoLogLength);
+		std::vector<char> rawLog(infoLogLength, '\0');
+		glGetShaderInfoLog(vertex, infoLogLength, &infoLogLength, rawLog.data());
+		std::ostringstream message;
+		message << "Vertex shader compilation failed!\n";
+		message << std::string{rawLog.data(), rawLog.data() + rawLog.size()};
+		throw std::runtime_error{message.str()};
+	}
+
+	GLuint fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	const char* fragmentSource = sources.fragment.c_str();
+	int fragmentLength = (int)sources.fragment.size();
+	glShaderSource(fragment, 1, &fragmentSource, &fragmentLength);
+	glCompileShader(fragment);
+	glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+	if (success == GL_FALSE)
+	{
+		int infoLogLength;
+		glGetShaderiv(fragment, GL_INFO_LOG_LENGTH, &infoLogLength);
+		std::vector<char> rawLog(infoLogLength, '\0');
+		glGetShaderInfoLog(fragment, infoLogLength, &infoLogLength, rawLog.data());
+		std::ostringstream message;
+		message << "Fragment shader compilation failed!\n";
+		message << std::string{rawLog.data(), rawLog.data() + rawLog.size()};
+		throw std::runtime_error{message.str()};
+	}
+
+	GLuint program = glCreateProgram();
+	glAttachShader(program, vertex);
+	glAttachShader(program, fragment);
+	glLinkProgram(program);
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (success == GL_FALSE)
+	{
+		int infoLogLength;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
+		std::vector<char> rawLog(infoLogLength, '\0');
+		glGetProgramInfoLog(program, infoLogLength, &infoLogLength, rawLog.data());
+		std::ostringstream message;
+		message << "Shader program linking failed!\n";
+		message << std::string{rawLog.data(), rawLog.data() + rawLog.size()};
+		throw std::runtime_error{message.str()};
+	}
+
+	glDetachShader(program, vertex);
+	glDetachShader(program, fragment);
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+	return Shader{program};
+}
+void phyto::engine::Shader::cleanup() const
+{
+	if (m_id != 0)
+	{
+		glDeleteProgram(m_id);
+	}
+}
+phyto::engine::Shader::~Shader()
+{
+	cleanup();
+}
+phyto::engine::Shader::Shader(phyto::engine::Shader&& other) noexcept : m_id{other.m_id}
+{
+	other.m_id = 0;
+}
+phyto::engine::Shader& phyto::engine::Shader::operator=(phyto::engine::Shader&& other) noexcept
+{
+	if (&other != this)
+	{
+		cleanup();
+		m_id = other.m_id;
+		other.m_id = 0;
+	}
+	return *this;
 }
